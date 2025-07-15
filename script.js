aa625697f031be1af0007f3644e4335c539add8f
saritoooh21 <castrosaramaribel@gmail.com>
2025-07-15 12:27:00 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,120 +2,119 @@ const materiasPorSemestre = [
   {
     semestre: "I",
     materias: [
-      "Lógica matemática",
-      "Programación básica",
-      "Proyecto de vida",
-      "Gestión básica de la información",
-      "Precálculo",
-      "Introducción a la ingeniería",
-      "CEPLEC I"
+      { nombre: "Gestión básica de la información", creditos: 3 },
+      { nombre: "CEPLEC I", creditos: 2 },
+      { nombre: "Precálculo", creditos: 3 },
+      { nombre: "Lógica matemática", creditos: 2 },
+      { nombre: "Introducción a la ingeniería", creditos: 1 },
+      { nombre: "Proyecto de vida", creditos: 2 },
+      { nombre: "Programación básica", creditos: 2 },
     ]
   },
   {
     semestre: "II",
     materias: [
-      "Inglés I",
-      "CEPLEC II",
-      "Cálculo diferencial",
-      "Álgebra lineal",
-      "Metodología de la investigación",
-      "Cátedra Minuto de Dios",
-      "Programación orientada a objetos"
+      { nombre: "Inglés I", creditos: 3 },
+      { nombre: "CEPLEC II", creditos: 2 },
+      { nombre: "Cálculo diferencial", creditos: 3 },
+      { nombre: "Álgebra lineal", creditos: 2 },
+      { nombre: "Metodología de la investigación", creditos: 2 },
+      { nombre: "Cátedra Minuto de Dios", creditos: 2 },
+      { nombre: "Programación orientada a objetos", creditos: 3 },
     ]
   },
   {
     semestre: "III",
     materias: [
-      "Inglés II",
-      "Cálculo integral",
-      "Física mecánica",
-      "Emprendimiento",
-      "Estructura de datos",
-      "Sistemas operativos"
+      { nombre: "Inglés II", creditos: 3 },
+      { nombre: "Cálculo integral", creditos: 3 },
+      { nombre: "Física mecánica", creditos: 3 },
+      { nombre: "Emprendimiento", creditos: 2 },
+      { nombre: "Estructura de datos", creditos: 3 },
+      { nombre: "Sistemas operativos", creditos: 3 },
     ]
   },
   {
     semestre: "IV",
     materias: [
-      "Inglés III",
-      "Física eléctrica",
-      "Desarrollo social contemporáneo",
-      "Redes de computadores",
-      "Bases de datos",
-      "Electiva CPC"
+      { nombre: "Inglés III", creditos: 3 },
+      { nombre: "Física eléctrica", creditos: 3 },
+      { nombre: "Desarrollo social contemporáneo", creditos: 2 },
+      { nombre: "Redes de computadores", creditos: 3 },
+      { nombre: "Bases de datos", creditos: 3 },
+      { nombre: "Electiva CPC", creditos: 2 },
     ]
   },
   {
     semestre: "V",
     materias: [
-      "Probabilidad y estadística",
-      "Práctica en responsabilidad social",
-      "Programación web",
-      "Ingeniería de software",
-      "Interconectividad",
-      "Sistemas transaccionales"
+      { nombre: "Probabilidad y estadística", creditos: 2 },
+      { nombre: "Práctica en responsabilidad social", creditos: 3 },
+      { nombre: "Programación web", creditos: 3 },
+      { nombre: "Ingeniería de software", creditos: 3 },
+      { nombre: "Interconectividad", creditos: 3 },
+      { nombre: "Sistemas transaccionales", creditos: 3 },
     ]
   },
   {
     semestre: "VI",
     materias: [
-      "Ecuaciones diferenciales",
-      "Electivas CMD Ingenieros sin fronteras",
-      "Desarrollo basado en plataformas",
-      "Diseño de algoritmos",
-      "Sistemas distribuidos",
-      "Arquitectura de datos",
-      "Electiva CPC"
+      { nombre: "Ecuaciones diferenciales", creditos: 2 },
+      { nombre: "Electiva CMD: Ingenieros sin fronteras", creditos: 3 },
+      { nombre: "Desarrollo basado en plataformas", creditos: 2 },
+      { nombre: "Diseño de algoritmos", creditos: 2 },
+      { nombre: "Sistemas distribuidos", creditos: 3 },
+      { nombre: "Arquitectura de datos", creditos: 3 },
+      { nombre: "Electiva CPC", creditos: 2 },
     ]
   },
   {
     semestre: "VII",
     materias: [
-      "Matemática discreta",
-      "Ética profesional",
-      "Electiva CMD",
-      "Arquitectura de software",
-      "Estructura de internet, servicios y servidores",
-      "Plan de negocios",
-      "Bases de datos masivas"
+      { nombre: "Matemática discreta", creditos: 2 },
+      { nombre: "Ética profesional", creditos: 2 },
+      { nombre: "Electiva CMD", creditos: 2 },
+      { nombre: "Arquitectura de software", creditos: 3 },
+      { nombre: "Estructura de internet, servicios y servidores", creditos: 3 },
+      { nombre: "Plan de negocios", creditos: 2 },
+      { nombre: "Bases de datos masivas", creditos: 3 },
     ]
   },
   {
     semestre: "VIII",
     materias: [
-      "Métodos numéricos",
-      "Electiva CMD",
-      "Electiva CP",
-      "Seguridad de la información",
-      "Ingeniería económica",
-      "Minería de datos",
-      "Electiva CPC"
+      { nombre: "Métodos numéricos", creditos: 2 },
+      { nombre: "Electiva CMD", creditos: 2 },
+      { nombre: "Electiva CP", creditos: 2 },
+      { nombre: "Seguridad de la información", creditos: 2 },
+      { nombre: "Ingeniería económica", creditos: 2 },
+      { nombre: "Minería de datos", creditos: 3 },
+      { nombre: "Electiva CPC", creditos: 3 },
     ]
   },
   {
     semestre: "IX",
     materias: [
-      "Seminario de investigación en ingeniería",
-      "Electiva CMD",
-      "Calidad y gestión de proyectos informáticos",
-      "Gestión de infraestructura",
-      "Electiva CPC",
-      "Práctica profesional"
+      { nombre: "Seminario de investigación en ingeniería", creditos: 1 },
+      { nombre: "Electiva CMD", creditos: 2 },
+      { nombre: "Calidad y gestión de proyectos informáticos", creditos: 2 },
+      { nombre: "Gestión de infraestructura", creditos: 3 },
+      { nombre: "Electiva CPC", creditos: 3 },
+      { nombre: "Práctica profesional", creditos: 6 },
     ]
   },
   {
     semestre: "X",
     materias: [
-      "Electiva CMD ingeniería y desarrollo regional",
-      "Electiva CP",
-      "Electiva CP",
-      "Electiva CPC",
-      "Opción de grado"
+      { nombre: "Electiva CMD: Ingeniería y desarrollo regional", creditos: 2 },
+      { nombre: "Electiva CP", creditos: 2 },
+      { nombre: "Electiva CP", creditos: 2 },
+      { nombre: "Electiva CPC", creditos: 3 },
+      { nombre: "Opción de grado", creditos: 5 },
     ]
   }
 ];
 
-// Renderizar la malla
 const contenedor = document.getElementById("malla");
 
 materiasPorSemestre.forEach((sem) => {
@@ -126,10 +125,16 @@ materiasPorSemestre.forEach((sem) => {
   sem.materias.forEach(materia => {
     const m = document.createElement("div");
     m.className = "materia";
-    m.textContent = materia;
+
+    m.innerHTML = `
+      <span class="nombre">${materia.nombre}</span>
+      <span class="creditos">${materia.creditos} cr</span>
+    `;
+
     m.addEventListener("click", () => {
       m.classList.toggle("completada");
     });
+
     div.appendChild(m);
   });
 
